76835b5ceb0a510eac6101dd9c33e20524eb8b39
chenbo <chenbo@szzt.com.cn>
2024-11-06 15:24:20 +0800

    feat: add zeus docker config

---
+++ zeus/mysql/docker-entrypoint-initdb.d/9_zeus_user.sql
@@ -0,0 +1,5 @@
+create user 'zeus'@'%' identified by '1qaz@wsx?S';
+
+GRANT ALL ON *.* TO 'zeus'@'%';
+
+flush privileges;
---
+++ zeus/redis/README.md
@@ -0,0 +1,4 @@
+redis 的配置文件 redis.conf 可以从以下地址获取：
+
+https://github.com/redis/redis/blob/7.0.14/redis.conf
+
