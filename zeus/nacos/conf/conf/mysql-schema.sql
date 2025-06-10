7b5d4e8f85c5360ed35abe63a94f563c35220c66
alexchen <alexchen.tech@gmail.com>
2025-06-10 10:18:07 +0800

    fix: update nacos version to 3.0.1

--- zeus/nacos/conf/conf/mysql-schema.sql
+++ zeus/nacos/conf/conf/mysql-schema.sql
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-use zeus_nacos;
+
 /******************************************/
 /*   表名称 = config_info                  */
 /******************************************/
