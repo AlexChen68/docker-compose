9671da90819066d4611f0da3d54e218277b9dc68
alexchen <alexchen.tech@gmail.com>
2026-03-05 17:20:09 +0800

    fix: 补充数据库初始化信息

--- mysql/stand-alone/docker-entrypoint-initdb.d/init.sql
+++ mysql/stand-alone/docker-entrypoint-initdb.d/init.sql
@@ -1,1 +1,7 @@
-create database `test` default character set utf8mb4 collate utf8mb4_0900_ai_ci;
+create database `zeus_test` default character set utf8mb4 collate utf8mb4_0900_ai_ci;
+
+create user 'zeus'@'%' identified by '1qaz@wsx?S';
+
+GRANT ALL ON *.* TO 'zeus'@'%';
+
+flush privileges;
